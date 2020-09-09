a2f405988218ddeab029f2257d7f7b3288ae16da
João Paulo Lima <jptick@gmail.com>
2020-09-09 19:28:01 -0300

    Conect API Backend with AXIOS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './App.css';
 
 import Header from './components/Header';
 
 function App() {
-  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response =>{
+      setProjects(response.data);
+    });
+  }, []);
 
   function handleAddProject(){
     setProjects([...projects, `Novo projeto ${Date.now()}`]);
@@ -15,7 +22,7 @@ function App() {
     <>
       <Header title="Projects" />
       <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
       <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
     </>
